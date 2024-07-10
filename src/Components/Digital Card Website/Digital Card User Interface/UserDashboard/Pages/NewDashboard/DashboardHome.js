593d17fa467bdf4dfac076379b4a6ad7dd4021b8
akshatJain <akshat@texlaculture.ai>
2024-07-10 22:29:40 +0530

    Akshat

--- src/Components/Digital Card Website/Digital Card User Interface/UserDashboard/Pages/NewDashboard/DashboardHome.js
+++ src/Components/Digital Card Website/Digital Card User Interface/UserDashboard/Pages/NewDashboard/DashboardHome.js
@@ -5,21 +5,17 @@ import NewBussinessProfileEcommerce from "../NewBussinessProfileEcommerce";
 import Newfooter from "../../../Components/NewFooter";
 import NewBussinessProfileHeader from "../NewBussinessProfileHeader";
 
-export default function DashboardHome()
-{
-    return(<Grid sx={{background:'#000',fontFamily:'Montserrat'}}>
-      <Grid>
-        <NewBussinessProfileHeader/>
-      </Grid>
-       <Grid>
-        <NewBussinessProfile/>
-       </Grid>
-       <Grid>
-       <NewBussinessProfileLink/>
-       </Grid>
-       <Grid>
-      <NewBussinessProfileEcommerce/>
-       </Grid>
+export default function DashboardHome() {
+  return (<Grid sx={{ background: '#000', fontFamily: 'Montserrat' }}>
+    <Grid>
+      <NewBussinessProfile />
+    </Grid>
+    <Grid>
+      <NewBussinessProfileLink />
+    </Grid>
+    <Grid>
+      <NewBussinessProfileEcommerce />
+    </Grid>
 
-    </Grid>)
+  </Grid>)
 }
